28497730ec2d50de1c801ce03723dedb627f39b1
Animesh Singh <animeshsingrol@gmail.com>
2024-05-06 23:43:48 +0530

    mongoosedb

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
 
 const app = express();
 
-
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World !");
+  res.send("Hello World !!");
 });
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`server running at ${PORT}`);
 });
